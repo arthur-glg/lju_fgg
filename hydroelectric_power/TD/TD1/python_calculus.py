540f433016d98765152b10112fbe4f35984158e9
arthur-glg <arthur.guillot-le-goff@etu.utc.fr>
2021-10-15 17:23:36 +0200

    15/10/2021
    - measurment : done
    - stability analysis : done
    - load combination : done
    - safety check : on going

---
+++ hydroelectric_power/TD/TD1/python_calculus.py
@@ -0,0 +1,34 @@
+'''Finding measurement'''
+H=71
+m=0.8
+B=m*H
+b0=H/10
+if b0<=8:
+    b0=8
+h0=b0/m
+'''Load combination'''
+#dead load
+gconc=24
+H0=H-6
+G1=b0*H*gconc
+G2=0.5*(B-b0)*(H-H0)*gconc
+rG1=B-(b0/2)
+rG2=(2/3)*(B-b0)
+#hydrostatic pressure
+gw=10
+Pw=gw*0.5*H0*H0
+rw=(1/3)*H0
+#uplift pressure
+gamma=0.85
+Pu=gamma*H0*gw*B/2
+ru=(2/3)*B
+#silt pressure
+gs=18
+Hs=H0/3
+Ps=gs*0.5*H0*H0
+rs=Hs/3
+'''security factors check'''
+nsl=(G1+G2-Pu)/(Pw+Ps)
+print("nsl=",nsl)
+nov=(G1*rG1+G2*rG2)/(Pw*rw+Ps*rs+Pu*ru)
+print("nov=",nov)
